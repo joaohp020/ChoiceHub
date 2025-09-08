d82f60fb7fdc7521d3546ccebbbbd02a0f9ffb3d
João Henrique <jh36750@gmail.com>
2025-09-08 18:17:44 -0300

    Implementando arquitetura DDD, e Entidades de Empresa

---
+++ ChoiceHub.Application/DTOs/EmpresaDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoiceHub.Application.DTOs
+{
+	public class EmpresaDto
+	{
+		public int Id { get; set; }
+		public string Cnpj { get; set; }
+		public string RazaoSocial { get; set; }
+	}
+}
---
+++ ChoiceHub.Domain/Entities/Empresa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoiceHub.Domain.Entities
+{
+	public class Empresa
+	{
+		public int Id { get; set; }
+		public string Cnpj { get; set; }
+		public string RazaoSocial { get; set; }
+		public string Email { get; set; }
+		public string Telefone { get; set; }
+		public string? Segmento { get; set; }
+		public int? Colaboradores { get; set; }
+		public int Status { get; set; }
+	}
+}
